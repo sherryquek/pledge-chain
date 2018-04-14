b528f95a6e3d8c9beeda046879013847c35e99e0
Joseph <josephchankh@gmail.com>
2018-04-14 15:56:58 +0800

    Done for profile page

--- pc-frontend/src/app/Profile/Profile.component.ts
+++ pc-frontend/src/app/Profile/Profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-profile',
@@ -7,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  title = 'app works!';
+  private apiUrl = 'http://localhost:3000/api/org.acme.model.User/alice%40foo';
+  data: any = {};
 
+  constructor(private http: Http, private route: ActivatedRoute) {
+    route.params.subscribe(params => { this.projId = params['projId'];});
+    console.log(this.projId);
+  	console.log('Hello fellow user');
+  	this.getContacts();
+  	this.getData();
+  }
+
+  getData() {
+  	return this.http.get(this.apiUrl)
+  		.map((res: Response) => res.json())
+  }
+
+  getContacts() {
+  	this.getData().subscribe(data => {
+  		console.log(data);
+  		this.data = data
+  	})
+  }
   ngOnInit() {
   }
 
