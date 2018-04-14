881d86871965cf7e9e47b7af91c5a1ff327b5468
Joseph <josephchankh@gmail.com>
2018-04-14 16:48:06 +0800

    Project, project-details page update

--- pc-frontend/src/app/ProjectDetails/ProjectDetails.component.ts
+++ pc-frontend/src/app/ProjectDetails/ProjectDetails.component.ts
@@ -15,24 +15,24 @@ export class ProjectDetailsComponent implements OnInit {
   }
 
   title = 'app works!';
-  private apiUrl = 'http://localhost:3000/api/org.acme.model.ProjectListing/0002';
+  private apiUrl = 'http://localhost:3000/api/org.acme.model.ProjectListing/';
   data: any = {};
 
   constructor(private http: Http, private route: ActivatedRoute) {
     route.params.subscribe(params => { this.projId = params['projId'];});
     console.log(this.projId);
   	console.log('Hello fellow user');
-  	this.getContacts();
-  	this.getData();
+  	this.getContacts(this.projId);
+  	this.getData(this.projId);
   }
 
-  getData() {
-  	return this.http.get(this.apiUrl)
+  getData(projId:String) {
+  	return this.http.get(this.apiUrl+projId)
   		.map((res: Response) => res.json())
   }
 
-  getContacts() {
-  	this.getData().subscribe(data => {
+  getContacts(projId:String) {
+  	this.getData(projId).subscribe(data => {
   		console.log(data);
   		this.data = data
   	})
